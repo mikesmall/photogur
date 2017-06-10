1c96801cb600caae974e85f80e20d90b600d391b
mikesmall <michael.l.a.small@gmail.com>
2017-06-10 13:11:59 -0400

    Completed step 'Making each picture show at its own URL with dynamic routes'

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 Rails.application.routes.draw do
 
   get 'pictures' => 'pictures#index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'pictures/:id' => 'pictures#show'
 
 end
