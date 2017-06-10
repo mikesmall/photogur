2fd11d14d55e7052dc5f8bd03646437954689aa0
mikesmall <michael.l.a.small@gmail.com>
2017-06-10 17:05:13 -0400

    Oops, THIS time I'm done 'Saving new pictures to the database'

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -13,7 +13,19 @@ class PicturesController < ApplicationController
   end#new
 
   def create
-    render text: "Received POST request to '/pictures' with the data URL: #{params}"
+    @picture = Picture.new(picture_params)
+    if @picture.save
+      # if the picture gets saved, generate a get request to "/pictures" (the index)
+      redirect_to "/pictures"
+    else
+      # otherwise render new.html.erb
+      render :new
+    end#if/else 
   end#create
 
-end
+
+  def picture_params
+    { title: params[:picture][:title], artist: params[:picture][:artist], url: params[:picture][:url] }
+  end
+
+end#PictureController class
